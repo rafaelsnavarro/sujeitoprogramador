9ad88c7610111e0dcbdd73ab74d1b5daaefafac4
Rafael S. Navarro <rafaelsnavarro26@gmail.com>
2022-12-06 18:26:09 -0300

    página de login finalizada

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,24 @@
+import './login.css'
+import { Logo } from '../../components/Logo'
+
 export default function Login(){
     return(
-        <div>
-            <h1>Página de Login</h1>
+        <div className="login-container">
+            <Logo/>
+            <form className="form">
+                <input
+                type="email"
+                placeholder="Digite seu email..."
+                />
+            
+            <input
+            type="password"
+            placeholder="********"
+            autoComplete="on"
+            />
+
+            <button type="submit">Acessar</button>
+            </form>
         </div>
     )
 }
