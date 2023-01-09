cf44e7a49b9cc6dbf30af82b5f56e91a0dbd54fb
unknown <devrafaelnavarro@outlook.com>
2023-01-09 14:35:06 -0300

    página admin finalizada e configurado interação com o banco de dados firebase

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,19 @@
 import './header.css'
 import { BiLogOut } from 'react-icons/bi'
 import { Link } from 'react-router-dom'
+import { auth } from '../../services/firebaseConnection'
+import { signOut } from 'firebase/auth'
 
 export function Header (){
-    async function handleLogout(){
-        alert("TESTE")
-    }
+
+        async function handleLogout(){
+            await signOut(auth)
+        }
 
     return(
         <header className="admin-header">
             <nav className="nav-header">
-                <button>
+                <button onClick={handleLogout}>
                     <BiLogOut size={28} color="#DB2629" />
                 </button>
                 <Link to="/admin">
